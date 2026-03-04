e8d97c97849f7d2d353909b420a5752c5662aef7
Selvi Shah <salushah97@gmail.com>
2026-03-04 15:46:47 +0530

    changes in util code

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+}).catch((err) => {
+    console.log("Mongo db connection failed!!", err);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,5 @@
+const asyncHandle = () => {
+
+}
+
+export {asyncHandle}
